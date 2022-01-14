39bab9f2aa7fc3fb90df4e034c0babcb167f48c9
YenXm <moonvoyager2@protonmail.com>
2022-01-14 14:00:58 -0500

    App icon

--- Rocket-Elevators-Mobile.Android/MainActivity.cs
+++ Rocket-Elevators-Mobile.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.Runtime;
 
 namespace Rocket_Elevators_Mobile.Droid
 {
-    [Activity(Label = "Rocket_Elevators_Mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "Rocket_Elevators_Mobile", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
